255fb09c610c95239cd27eb1624a2d239625a063
Junior <junior.forneiro@brasil317.com.br>
2022-08-01 08:23:19 -0300

    JavaScript Básico primeiro commit

---
+++ HTML e CSS para Iniciantes/13_JavaScript_basico/script.js
@@ -0,0 +1,19 @@
+const curso = "JavaScript";
+
+let total = 10;
+total = 20;
+
+console.log(curso);
+console.log(total);
+
+const nav = document.querySelector("nav");
+const produtos = document.querySelector(".produtos a");
+
+console.log(nav);
+console.log(produtos);
+console.log(produtos.innerHTML);
+console.log(produtos.href);
+
+// produtos.remove();
+nav.style.backgroundColor = "black";
+nav.style.padding = "1rem";
